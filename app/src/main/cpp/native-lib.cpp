29111d1d574edc30723510b7fd86d6fed3d360b5
rodrigues882013 <rodrigues882007@gmail.com>
2017-07-15 02:03:59 -0300

    -- change native function

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -4,9 +4,11 @@
 
 #include "native-lib.h"
 
+double DOLLAR_VALUE = 3.5;
+
 JNIEXPORT jdouble JNICALL
 Java_com_felipe_app_services_NativeClient_callbackToNative(JNIEnv *env, jobject obj, jdouble num1)
 {
-    //Simple survey operation, convert dollar (US) to real (BRL)
-    return num1*3.5;
+    //Simple currency operation, convert dollar (US) to real (BRL)
+    return num1*DOLLAR_VALUE;
 }
